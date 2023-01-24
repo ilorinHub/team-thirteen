aca27db64520f3ef13ffd21ac3277f5b1ab6a7cd
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-01-24 16:29:29 +0100

    feat: adds theme provider and global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../src/styles/globals.css'
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../src/theme/theme'
+import { GlobalStyles } from '../src/theme/globalStyles';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
