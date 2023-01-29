fc7b9c2d1cb04fce7ecffb7f9c5d5ef1bd64eece
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-01-29 08:45:23 +0100

    file added

---
+++ pages/projects.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Projects from '../src/components/dashboardComponents/projects/Projects'
+
+function projects() {
+    return (
+        <Projects />
+    )
+}
+
+export default projects
